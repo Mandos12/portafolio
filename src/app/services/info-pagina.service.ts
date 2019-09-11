0abe21fc4d4666128754cf2bd0fb86038201e43a
Armando Hernandez <hernandezjuarezjosearmando@gmail.com>
2019-09-11 02:32:01 -0500

    Se agregan peticiones http e interfaces

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) { 
+
+    console.log('Servicio de info pagina listo');
+
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (response: InfoPagina) => {
+
+        this.cargada = true;
+        this.info = response;
+
+        console.log(response);
+
+      })
+
+   }
+}
