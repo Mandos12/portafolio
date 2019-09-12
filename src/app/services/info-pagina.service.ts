2bd13017c90011c2f80803e5edbaa41011cefbc3
Armando Hernandez <hernandezjuarezjosearmando@gmail.com>
2019-09-12 00:01:34 -0500

    Se agregan productos de firebase y tambien un loading

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,10 +10,16 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
 
-    console.log('Servicio de info pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (response: InfoPagina) => {
@@ -21,9 +27,13 @@ export class InfoPaginaService {
         this.cargada = true;
         this.info = response;
 
-        console.log(response);
+      });
+  }
 
-      })
-
-   }
+  private cargarEquipo(){
+    this.http.get('https://angular-html-9e09c.firebaseio.com/equipo.json')
+      .subscribe( (response: any) => {
+        this.equipo = response;
+      });
+  }
 }
