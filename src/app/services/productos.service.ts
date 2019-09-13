4ad4af5099517b71c6a0b83d94243b42b7bb73d8
Armando Hernandez <hernandezjuarezjosearmando@gmail.com>
2019-09-13 01:54:40 -0500

    Listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
   
@@ -17,13 +18,54 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-9e09c.firebaseio.com/productos_idx.json')
-    .subscribe( (response: Producto[]) => {
 
-      console.log(response);
-      this.productos = response;
-      this.cargando = false;
+    return new Promise( ( resolve, reject ) => {
+
+      this.http.get('https://angular-html-9e09c.firebaseio.com/productos_idx.json')
+      .subscribe( (response: Producto[]) => {
+        this.productos = response;
+        this.cargando = false;
+        resolve();
+      });
 
     });
   }
+
+  getProducto( id: string ) {
+    return this.http.get(`https://angular-html-9e09c.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(  termino: string ) {
+
+    if(this.productos.length === 0) {
+      // Cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+  
+  private filtrarProductos( termino: string ) {
+    
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+
+  }
+
 }
